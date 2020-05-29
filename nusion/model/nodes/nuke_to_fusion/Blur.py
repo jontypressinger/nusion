0ae7a9bb236e59033187a664b5016555d2e4aa2c
Jonty <jpressinger@gmail.com>
2020-05-29 18:22:00 +0200

    scratch formula in comments

--- nusion/model/nodes/nuke_to_fusion/Blur.py
+++ nusion/model/nodes/nuke_to_fusion/Blur.py
@@ -1,3 +1,11 @@
+"""
+
+1k square 10blur to 4096 50blur:
+6.890909 * 5 = 34.454545
+4096/1000 = 4.096
+34.454545 / 4.096 = 8.41175415039
+
+"""
 
 def convert(nuke_effect_attribs):
     """ Convert Nuke Blur to Fusion Blur
@@ -14,12 +22,12 @@ def convert(nuke_effect_attribs):
             if value.startswith("{"):
                 fusion_effect_attribs["LockXY"] = "Input {Value = 0, }"
                 value = value.replace("{", "").replace("}", "").split(" ")
-                fusion_value_x = round(int(value[0]) / 1.9029, 3)
-                fusion_value_y = round(int(value[1]) / 1.9029, 3)
+                fusion_value_x = round(int(value[0]) / 1.9029, 5)
+                fusion_value_y = round(int(value[1]) / 1.9029, 5)
                 fusion_effect_attribs["XBlurSize"] = f"Input {{ Value = {fusion_value_x}, }}"
                 fusion_effect_attribs["YBlurSize"] = f"Input {{ Value = {fusion_value_y}, }}"
             else:
-                fusion_value = round(int(value) / 1.9029, 3)
+                fusion_value = round(int(value) / 1.9029, 5)
                 fusion_effect_attribs["XBlurSize"] = f"Input {{ Value = {fusion_value}, }}"
 
     return fusion_effect_attribs
