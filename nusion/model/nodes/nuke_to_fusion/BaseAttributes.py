676c34164a6babe1f8f9e1e0adc4b84db710ae77
Jonty <jpressinger@gmail.com>
2020-05-31 20:20:00 +0200

    add base attribute convert file

---
+++ nusion/model/nodes/nuke_to_fusion/BaseAttributes.py
@@ -0,0 +1,14 @@
+
+def convert(nuke_base_attribs):
+    """ Convert Nuke Base Attributes to Fusion Base Attributes
+
+    Returns:
+        dict with fusion formatted base attributes.
+    """
+    fusion_base_attribs = {}
+
+    fusion_base_attribs[0] = "This is currently a placeholder effect."
+    return fusion_base_attribs
+
+if __name__ == '__main__':
+    help(convert)
