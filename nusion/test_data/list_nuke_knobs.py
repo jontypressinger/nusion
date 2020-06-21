bbba1372cdc34ee49b923b449e60e55f9baa0b55
Jonty <jpressinger@gmail.com>
2020-06-21 21:03:00 +0200

    add base and common attributes to nuke test script

--- nusion/test_data/list_nuke_knobs.py
+++ nusion/test_data/list_nuke_knobs.py
@@ -1,6 +1,44 @@
+# pylint: skip-file
 """
 Run in nuke script editor to list all knobs on selected node.
 """
 
+already_known_list = [
+    "name",
+    "xpos",
+    "ypos",
+    "disable",
+    "postage_stamp",
+    "tile_color",
+    "note_font_color",
+    "note_font_size",
+    "useLifetime",
+    "lifetimeStart",
+    "lifetimeEnd",
+    "mix",
+    "channels",
+    "label",
+    "help",
+    "dope_sheet",
+    "hide_input",
+    "onCreate",
+    "updateUI",
+    "rootNodeUpdated",
+    "knobChanged",
+    "note_font",
+    "bookmark",
+    "selected",
+    "autolabel",
+    "onDestroy",
+    "indicators",
+    "icon",
+    "enable",
+    "postage_stamp_frame",
+    "panel",
+    "cached",
+    "gl_color"
+    ]
+
 for knob in nuke.selectedNode().knobs():
-    print knob
+    if knob not in already_known_list:
+        print(knob)
