26f0897da720dec96f07f95270ce120f96fbc2bc
Jonty <jpressinger@gmail.com>
2020-06-29 22:44:55 +0200

    add gamma and offset conversion

--- nusion/model/nodes/nuke_to_fusion/ColorCorrect.py
+++ nusion/model/nodes/nuke_to_fusion/ColorCorrect.py
@@ -16,9 +16,9 @@ def convert(node):
         if knob == "gain":
             if value.startswith("{"): # Multiple channels
                 split_value = value.replace("{", "").replace("}", "").split(" ")
-                fusion_effect_attribs["MasterRedGain"] = f"Input {{Value = {split_value[1]}, }}"
-                fusion_effect_attribs["MasterGreenGain"] = f"Input {{Value = {split_value[2]}, }}"
-                fusion_effect_attribs["MasterBlueGain"] = f"Input {{Value = {split_value[3]}, }}"
+                fusion_effect_attribs["MasterRedGain"] = f"Input {{Value = {split_value[0]}, }}"
+                fusion_effect_attribs["MasterGreenGain"] = f"Input {{Value = {split_value[1]}, }}"
+                fusion_effect_attribs["MasterBlueGain"] = f"Input {{Value = {split_value[2]}, }}"
             else: #Master channel
                 fusion_effect_attribs["MasterRGBGain"] = f"Input {{Value = {value}, }}"
 
@@ -30,13 +30,66 @@ def convert(node):
             if value.startswith("{"): # Multiple channels
                 split_value = value.replace("{", "").replace("}", "").split(" ")
                 fusion_effect_attribs[f"{fusion_knob}RedGain"] = \
-                    f"Input {{Value = {split_value[1]}, }}"
+                    f"Input {{Value = {split_value[0]}, }}"
                 fusion_effect_attribs[f"{fusion_knob}GreenGain"] = \
-                    f"Input {{Value = {split_value[2]}, }}"
+                    f"Input {{Value = {split_value[1]}, }}"
                 fusion_effect_attribs[f"{fusion_knob}BlueGain"] = \
-                    f"Input {{Value = {split_value[3]}, }}"
+                    f"Input {{Value = {split_value[2]}, }}"
             else: #Master channel
                 fusion_effect_attribs[f"{fusion_knob}RGBGain"] = f"Input {{Value = {value}, }}"
+
+        if knob == "gamma":
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                fusion_effect_attribs["MasterRedGamma"] = f"Input {{Value = {split_value[0]}, }}"
+                fusion_effect_attribs["MasterGreenGamma"] = f"Input {{Value = {split_value[1]}, }}"
+                fusion_effect_attribs["MasterBlueGamma"] = f"Input {{Value = {split_value[2]}, }}"
+            else: #Master channel
+                fusion_effect_attribs["MasterRGBGamma"] = f"Input {{Value = {value}, }}"
+
+        if ".gamma" in knob:
+            #TODO: Raise warning about ranges curve making this conversion not 100% accurate.
+            fusion_knob = knob.split(".")[0].capitalize()
+            if fusion_knob == "Midtones":
+                fusion_knob = "MidTones" #Exception for Fusion's weird capitalisation
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                fusion_effect_attribs[f"{fusion_knob}RedGamma"] = \
+                    f"Input {{Value = {split_value[0]}, }}"
+                fusion_effect_attribs[f"{fusion_knob}GreenGamma"] = \
+                    f"Input {{Value = {split_value[1]}, }}"
+                fusion_effect_attribs[f"{fusion_knob}BlueGamma"] = \
+                    f"Input {{Value = {split_value[2]}, }}"
+            else: #Master channel
+                fusion_effect_attribs[f"{fusion_knob}RGBGamma"] = f"Input {{Value = {value}, }}"
+
+        if knob == "offset":
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                fusion_effect_attribs["MasterRedBrightness"] = f"Input {{Value = {split_value[0]}, }}"
+                fusion_effect_attribs["MasterGreenBrightness"] = f"Input {{Value = {split_value[1]}, }}"
+                fusion_effect_attribs["MasterBlueBrightness"] = f"Input {{Value = {split_value[2]}, }}"
+            else: #Master channel
+                fusion_effect_attribs["MasterRGBBrightness"] = f"Input {{Value = {value}, }}"
+
+        if ".offset" in knob:
+            #TODO: Raise warning about ranges curve making this conversion not 100% accurate.
+            fusion_knob = knob.split(".")[0].capitalize()
+            if fusion_knob == "Midtones":
+                fusion_knob = "MidTones" #Exception for Fusion's weird capitalisation
+            if value.startswith("{"): # Multiple channels
+                split_value = value.replace("{", "").replace("}", "").split(" ")
+                fusion_effect_attribs[f"{fusion_knob}RedBrightness"] = \
+                    f"Input {{Value = {split_value[0]}, }}"
+                fusion_effect_attribs[f"{fusion_knob}GreenBrightness"] = \
+                    f"Input {{Value = {split_value[1]}, }}"
+                fusion_effect_attribs[f"{fusion_knob}BlueBrightness"] = \
+                    f"Input {{Value = {split_value[2]}, }}"
+            else: #Master channel
+                fusion_effect_attribs[f"{fusion_knob}RGBBrightness"] = f"Input {{Value = {value}, }}"
+
+        #Match nuke default process order (Gamma before levels)
+        fusion_effect_attribs["ProcessOrder"] = "Input { Value = 0, }"
 
         #Match nuke default Ranges curve
         fusion_effect_attribs["ColorRanges"] = \
